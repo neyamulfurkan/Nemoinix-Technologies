801491f08dad46985a3105d48084fdba17edf1e2
nemionix <neyamulfurkan22@gmail.com>
2026-01-05 08:28:02 +0600

    Add frontend URL to CORS whitelist

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,7 @@ const corsOptions = {
             'http://localhost:3000',
             'http://127.0.0.1:3000',
             'https://nemionix-technologies.onrender.com',
+            'https://nemoinix-technologies.onrender.com',
             process.env.FRONTEND_URL
         ].filter(Boolean);
         
